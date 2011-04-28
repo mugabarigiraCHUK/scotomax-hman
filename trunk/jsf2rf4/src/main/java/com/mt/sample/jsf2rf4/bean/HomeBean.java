57e303375a1d7c657597953b01ab20c5fb023669
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-04-28 12:19:08 +0000

    First commit with all components.

---
+++ trunk/jsf2rf4/src/main/java/com/mt/sample/jsf2rf4/bean/HomeBean.java
@@ -0,0 +1,11 @@
+package com.mt.sample.jsf2rf4.bean;
+
+import lombok.Data;
+
+public @Data abstract class HomeBean {
+
+	protected String username;
+	protected String password;
+	protected String message;
+	
+}
