8828d03644c9c7d597a0eeadd6cede8544f18dc8
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-04-28 12:19:08 +0000

    First commit with all components.

---
+++ jsf2rf4/src/main/java/com/mt/sample/jsf2rf4/HomeController.java
@@ -0,0 +1,28 @@
+package com.mt.sample.jsf2rf4;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mt.sample.jsf2rf4.bean.HomeBean;
+
+@ManagedBean
+@ViewScoped
+public class HomeController extends HomeBean {
+
+	final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	public void login(){
+		logger.debug("Entering login method.");
+		message = "Login successfully, welcome " + username;
+	}
+	
+	public void logout(){
+		logger.debug("Entering logout method.");
+		username = "";
+		password = "";
+		message = "Logout successfully";
+	}
+}
