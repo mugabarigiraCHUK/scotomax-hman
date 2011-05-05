37f04b823bac67728b7200575925d779fca16f99
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-05-05 09:23:51 +0000

    Initial project components

---
+++ trunk/callcenter/src/main/java/com/itap/controllers/beans/HomeBean.java
@@ -0,0 +1,11 @@
+package com.itap.controllers.beans;
+
+import lombok.Data;
+
+public @Data abstract class HomeBean {
+
+	protected String username;
+	protected String password;
+	protected String message;
+	
+}
