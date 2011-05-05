dded1ed108cd70dfbc514aeda5d3cf7cb67161ae
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-05-05 17:43:23 +0000

    First commit for initial project component.

---
+++ tasks/app/controllers/Application.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.util.*;
+
+import models.*;
+import play.mvc.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+    	List<Task> tasks = Task.find("order by id desc").fetch();
+        render(tasks);
+    }
+    
+    public static void createTask(String title) {
+    	Task task = new Task(title).save();
+    	renderJSON(task);
+    }
+    
+    public static void changeStatus(Long id, boolean done) {
+    	Task task = Task.findById(id);
+    	task.done = done;
+    	task.save();
+    	renderJSON(task);
+    }
+
+}
