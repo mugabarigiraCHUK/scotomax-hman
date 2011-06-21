d3180cb21b2785f4e15780cee3ad43970dfa4ed8
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-06-21 18:00:03 +0000

    Added jQuery & jQuery UI

---
+++ hman-php/application/views/template.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Hman-PHP revision 1 built 999</title>
+		<!-- Default CSS and JavaScript -->
+		<link type="text/css" href="css/flick/jquery-ui-1.8.13.custom.css" rel="stylesheet" />	
+		<script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
+		<!-- Default CSS and JavaScript -->
+		<link type="text/css" href="css/template.css" rel="stylesheet" />
+		<script type="text/javascript" src="js/template.js"></script>
+	</head>
+	<body>
+		
+    </body>
+</html>
