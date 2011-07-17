d5287b8ce82ab53d1f6b19fb500e6e8091b71cf6
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-07-17 08:21:18 +0000

    Update CSS and layout.

--- trunk/hman-php/application/controllers/home.php
+++ trunk/hman-php/application/controllers/home.php
@@ -15,7 +15,9 @@ class Home extends CI_Controller {
 	}
 
 	public function index() {
+		$this->load->view('template/header_view');
 		$this->load->view('home_view');
+		$this->load->view('template/footer_view');
 	}
 
 }
---
+++ trunk/hman-php/application/views/template/footer_view.php
@@ -0,0 +1,48 @@
+					</p>
+				</div>
+
+				<!-- Navigation contant page -->
+				<div id="nav">
+					<p>
+						<div class="ui-state-default ui-corner-all icon-grip">
+							<span class="ui-icon ui-icon-grip-diagonal-se"></span>
+						</div>
+						&nbsp;Content<hr/>
+					</p>
+					<p>
+						<!--
+							Navigate content page..
+						-->
+						<table width="100%">
+							<tr>
+								<td>
+									<div class="ui-state-default ui-corner-all icon-grip"><span class="ui-icon ui-icon-newwin"></span></div>
+						  			<div style="float: left;">&nbsp;Today</div>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<div class="ui-state-default ui-corner-all icon-grip"><span class="ui-icon ui-icon-newwin"></span></div>
+						  			<div style="float: left;">&nbsp;Weekly</div>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<div class="ui-state-default ui-corner-all icon-grip"><span class="ui-icon ui-icon-newwin"></span></div>
+						  			<div style="float: left;">&nbsp;Monthly</div>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<div class="ui-state-default ui-corner-all icon-grip"><span class="ui-icon ui-icon-newwin"></span></div>
+						  			<div style="float: left;">&nbsp;All</div>
+								</td>
+							</tr>
+						</table>
+					</p>
+				</div>
+			</div>
+		</div>
+		<?php include("assets/theme/footer.html"); ?>
+    </body>
+</html>
