d523a6a8cc46298c50f12e6b82e6b585404d78a0
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-07-17 04:20:03 +0000

    Update CSS and layout.

---
+++ trunk/hman-php/assets/js/global.js
@@ -0,0 +1,35 @@
+/**
+  * Java Script
+  *
+  */
+var bmenu = {
+	bmenus : ["home", "note", "profile", "contacts","setting"],	
+	_init : function() {
+		// Button mouse over event handle.		
+		$('.mbutton').hover(function(){
+			$(this).addClass("mbutton_hover");
+		}, function(){
+			$(this).removeClass("mbutton_hover");
+		});
+		// Button mouse click event handle.
+		$('#home').click(function(){
+			alert("Entering home page.");
+		});
+
+		$('#note').click(function(){
+			alert("Entering note page.");
+		});
+
+		$('#profile').click(function(){
+			alert("Entering profile page.");
+		});
+
+		$('#contacts').click(function(){
+			alert("Entering contact page.");
+		});
+
+		$('#setting').click(function(){
+			alert("Entering setting page.");
+		});
+	}
+};
