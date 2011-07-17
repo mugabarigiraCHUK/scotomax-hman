c9152a9091e3b9d6b3d9c9f7271b0b1e136b5513
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-07-17 08:21:18 +0000

    Update CSS and layout.

---
+++ hman-php/application/views/template/header_view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<title>Hman-PHP revision 1 built 999</title>
+		<?php include("assets/theme/head.html"); ?>
+	</head>
+	<body>
+		<?php include("assets/theme/header.html"); ?>
+		<div id="container">
+			<div id="menu">
+				<!--
+					Main menus content page..
+				-->
+				<div id="home" class="mbutton">Home</div>
+				<div id="note" class="mbutton">Note</div>
+				<div id="profile" class="mbutton">Profile</div>
+				<div id="contacts" class="mbutton">Contacts</div>
+				<div id="setting" class="mbutton">Setting</div>
+			</div>
+			<div id="content">
+				<!-- content -->
+				<div id="data">
+					<p>
+						<div class="ui-state-default ui-corner-all icon-grip">
+							<span class="ui-icon ui-icon-grip-diagonal-se"></span>
+						</div>
+						&nbsp;Information<hr/>
+					</p>
+					<!-- Information contant page -->
+					<p>
