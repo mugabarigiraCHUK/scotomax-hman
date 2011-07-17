acc2609b9e4ddd135dd1a7c74b7ffb7b0389f2d2
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-07-17 04:22:45 +0000

    Update CSS and layout.

---
+++ hman-php/application/libraries/hutils.php
@@ -0,0 +1,19 @@
+<?php
+
+class HUtils {
+
+	public function twit( $message) {
+		return TRUE;
+	}
+
+	public function fb_post($message) {
+		return TRUE;
+	}
+
+	public function gplus_post($message) {
+		return TRUE;
+	}
+
+}
+
+?>
