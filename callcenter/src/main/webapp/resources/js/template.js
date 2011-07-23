924a82c4e9756d7127b1f083bb18ef4632de7ca4
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-07-23 10:10:22 +0000

    Template CSS.

--- callcenter/src/main/webapp/resources/js/template.js
+++ callcenter/src/main/webapp/resources/js/template.js
@@ -2,4 +2,25 @@ $(function(){
 	$(".cmdLogin").click(function(){
 		window.location.href='login.faces';
 	});
+	
+	$("#myphone").click(function(){
+		alert('Go to My Phone information page.');
+	});
+	
+	$("#datacenter").click(function(){
+		alert('Go to Data Center information page.');
+	});
+	
+	$("#mycalendar").click(function(){
+		alert('Go to My Calendar information page.');
+	});
+	
+	$("#myagent").click(function(){
+		alert('Go to My Agent information page.');
+	});
+	
+	$("#myjob").click(function(){
+		alert('Go to My Jobs information page.');
+	});
+	
 });
