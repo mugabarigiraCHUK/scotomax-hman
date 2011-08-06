e21645a32d123562d4c68c2ac3991ea71dc74533
phamonyut <phamonyut@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-08-06 13:28:24 +0000

    add test bean validate

--- trunk/callcenter/src/test/java/com/itap/callcenter/dao/impl/apc/DaoImplTest.java
+++ trunk/callcenter/src/test/java/com/itap/callcenter/dao/impl/apc/DaoImplTest.java
@@ -1,5 +1,15 @@
 package com.itap.callcenter.dao.impl.apc;
 
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,23 +20,50 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.itap.callcenter.dao.apc.acd.AcdConditionDao;
 import com.itap.callcenter.dao.apc.acd.AcdRouteDao;
+import com.itap.callcenter.dao.apc.knw.KnwTopicDao;
+import com.itap.callcenter.entity.apc.acd.AcdCondition;
 
 /**
  * 
  * @author scotomax
- *
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:spring-test.xml")
+@ContextConfiguration(locations = "classpath:spring-test.xml")
 @TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
 public class DaoImplTest {
 
-	@Autowired AcdConditionDao acdConditionDao;
-	@Autowired AcdRouteDao acdRouteDao;
-	
+	@Autowired
+	AcdConditionDao acdConditionDao;
+	@Autowired
+	AcdRouteDao acdRouteDao;
+	@Autowired
+	KnwTopicDao knwTopicDao;
+
 	@Test
-	@Transactional
 	public void acdConditionDaoTest() {
-		
+		System.out.println("test acdConditionDaoTest");
+		System.out.println(acdRouteDao);
+		System.out.println(knwTopicDao);
 	}
+
+	@Test
+	public void validateBean() {
+		System.out.println("test validateBean start");
+		AcdCondition acdCondition = new AcdCondition();
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<AcdCondition>> constraintViolations = validator.validate(acdCondition);
+
+		Iterator<ConstraintViolation<AcdCondition>> iter = constraintViolations.iterator();
+		assertEquals(3, constraintViolations.size());
+		assertEquals("may not be null", iter.next().getMessage());
+		assertEquals("may not be null", iter.next().getMessage());
+		assertEquals("may not be null", iter.next().getMessage());
+		
+		System.out.println("test validateBean finish");
+	}
+
 }
