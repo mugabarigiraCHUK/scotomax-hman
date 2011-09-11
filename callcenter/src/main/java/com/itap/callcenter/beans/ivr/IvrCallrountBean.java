b884e22cafc8b5c9bc5ba5183422af6d061c9350
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-09-11 14:34:45 +0000

    Component page of IVR call route data management.

--- callcenter/src/main/java/com/itap/callcenter/beans/ivr/IvrCallrountBean.java
+++ callcenter/src/main/java/com/itap/callcenter/beans/ivr/IvrCallrountBean.java
@@ -12,14 +12,16 @@ import lombok.Data;
  */
 public @Data abstract class IvrCallrountBean implements Serializable {
 	
+	protected static final long serialVersionUID = -1288920131638566740L;
+	
 	protected Integer callrouteId;
 	protected String callrouteName;
 	protected String callrouteDescription;
 	protected String callrouteCaller;
 	protected String callrouteCalled;
+	
 	protected Date callrouteCreateDate;
 	protected Date callrouteUpdateDate;
-
 	
-	
+	protected Integer selectedCallrouteId;
 }
