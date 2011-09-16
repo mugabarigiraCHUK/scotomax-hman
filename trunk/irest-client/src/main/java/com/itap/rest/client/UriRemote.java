4317e33180d37d09a0904b99fb7a2fab7020c0e2
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-09-16 06:02:12 +0000

    Project components

---
+++ trunk/irest-client/src/main/java/com/itap/rest/client/UriRemote.java
@@ -0,0 +1,16 @@
+package com.itap.rest.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author scotomax
+ *
+ */
+public class UriRemote {
+
+	Logger log4j = LoggerFactory.getLogger(UriRemote.class);
+	
+	
+}
