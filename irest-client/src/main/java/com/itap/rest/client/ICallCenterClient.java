d81a53c91e37c863a3e75b1dda89c28660e0aa2d
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-09-17 14:41:06 +0000

    Update REST communication by XML pass through.

--- irest-client/src/main/java/com/itap/rest/client/ICallCenterClient.java
+++ irest-client/src/main/java/com/itap/rest/client/ICallCenterClient.java
@@ -4,16 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.httpclient.Credentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
+import com.itap.rest.client.bean.XmlIvrRequest;
 import com.itap.rest.client.bean.XmlIvrResponse;
 
+/**
+ * 
+ * @author scotomax
+ *
+ */
 public class ICallCenterClient {
+	
+	Logger logger = LoggerFactory.getLogger(ICallCenterClient.class);
 
 	private final RestTemplate restTemplate;
 	private final Credentials credentials;
 	private final UriRemote uriRemote;
 	
+	/*
+	 * Constructor method
+	 */
 	public ICallCenterClient(RestTemplate restTemplate, 
 															Credentials credentials,
 															UriRemote uriRemote){
@@ -25,8 +38,16 @@ public class ICallCenterClient {
         HttpClient client = factory.getHttpClient();
         client.getState().setCredentials(AuthScope.ANY, credentials);
         */
+		logger.debug("Look->Credentials->" + credentials);
 	}
 	
+	/**
+	 * REST services for authentication
+	 * 
+	 * @param usrname
+	 * @param passwd
+	 * @return XmlIvrResponse.class
+	 */
 	public XmlIvrResponse authService(String usrname, String passwd){
 		Map<String, String> vars = new HashMap<String, String>();
         vars.put("usrname", "admin");
@@ -34,9 +55,25 @@ public class ICallCenterClient {
         return restTemplate.getForObject(uriRemote.getUriAuth(), XmlIvrResponse.class, vars);
 	}
 	
+	/**
+	 * REST services for checking the services
+	 * 
+	 * @return XmlIvrResponse.class
+	 */
 	public XmlIvrResponse checkService(){
 		Map<String, String> vars = new HashMap<String, String>();
         return restTemplate.getForObject(uriRemote.getUriCheck(), XmlIvrResponse.class, vars);
+	}
+	
+	/**
+	 * REST services for try to post XML from client into the server
+	 * 
+	 * @param request
+	 * @return XmlIvrResponse.class
+	 */
+	public XmlIvrResponse callback(XmlIvrRequest request) {
+		Map<String, String> vars = new HashMap<String, String>();
+		return restTemplate.postForObject(uriRemote.getUriCallback(), request, XmlIvrResponse.class, vars);
 	}
 	
 }
--- irest-client/src/test/java/com/itap/rest/client/ICallCenterClientTest.java
+++ irest-client/src/test/java/com/itap/rest/client/ICallCenterClientTest.java
@@ -1,5 +1,8 @@
 package com.itap.rest.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -8,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.itap.rest.client.bean.XmlIvrParam;
+import com.itap.rest.client.bean.XmlIvrParams;
+import com.itap.rest.client.bean.XmlIvrRequest;
 import com.itap.rest.client.bean.XmlIvrResponse;
 
 /**
@@ -26,12 +32,43 @@ public class ICallCenterClientTest {
 	
 	@Test
 	public void execute(){
-		log4j.info("Entering execute method to running unit test-case.");
+		log4j.info(" *** Entering execute Authenticate REST services.");
 		XmlIvrResponse response = iCallCenterClient.authService("admin", "adminadmin");
 		
 		log4j.info("Status->Code->" + response.getStatus().getResponseCode());
 		log4j.info("Status->Desc->" + response.getStatus().getResponseDesc());
 		
+	}
+	
+	@Test
+	public void callback(){
+		log4j.info(" *** Entering execute Call-back REST services.");
+		
+		XmlIvrRequest request = new XmlIvrRequest();
+		XmlIvrParams params = new XmlIvrParams();
+		List<XmlIvrParam> paramList = new ArrayList<XmlIvrParam>();
+		
+		XmlIvrParam param = new XmlIvrParam();
+		param.setName("fullname");
+		param.setValue("Mr. Steve Job");
+		paramList.add(param);
+		
+		param = new XmlIvrParam();
+		param.setName("mobile");
+		param.setValue("99813747473");
+		paramList.add(param);
+		
+		param = new XmlIvrParam();
+		param.setName("dollar");
+		param.setValue("120");
+		paramList.add(param);
+		request.setParams(params);
+		
+		XmlIvrResponse response = iCallCenterClient.callback(request);
+		
+		log4j.info("Status->Code->" + response.getStatus().getResponseCode());
+		log4j.info("Status->Desc->" + response.getStatus().getResponseDesc());
+	
 	}
 	
 }
