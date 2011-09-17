e9875d33c92412c3a75c69b94668c24d34e1a606
developmax <developmax@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-09-17 14:41:48 +0000

    Update REST communication by XML pass through.

--- trunk/callcenter/src/main/java/com/itap/callcenter/mvc/controller/IvrServicesController.java
+++ trunk/callcenter/src/main/java/com/itap/callcenter/mvc/controller/IvrServicesController.java
@@ -11,16 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.itap.callcenter.mvc.bean.XmlIvrParam;
 import com.itap.callcenter.mvc.bean.XmlIvrParams;
+import com.itap.callcenter.mvc.bean.XmlIvrRequest;
 import com.itap.callcenter.mvc.bean.XmlIvrResponse;
 import com.itap.callcenter.mvc.bean.XmlIvrStatus;
 
@@ -39,6 +43,7 @@ public class IvrServicesController implements Serializable {
 	Logger logger = LoggerFactory.getLogger(IvrServicesController.class);
 	
 	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ResponseBody
 	public XmlIvrResponse exception(Exception ex, HttpServletRequest req, HttpServletResponse resp) {
 		
@@ -56,6 +61,7 @@ public class IvrServicesController implements Serializable {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, value="/auth/{username}/{passwd}")
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	@ResponseBody
 	public XmlIvrResponse  authenticate(@PathVariable("username") String username, 
 																						@PathVariable("passwd") String passwd, 
@@ -98,6 +104,7 @@ public class IvrServicesController implements Serializable {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, value="/check")
+	@ResponseStatus(HttpStatus.ALREADY_REPORTED)
 	@ResponseBody
 	public XmlIvrResponse handcheck(Model model) {
 		logger.debug("Entering handcheck)() method...");
@@ -135,6 +142,37 @@ public class IvrServicesController implements Serializable {
 		XmlIvrStatus status = new XmlIvrStatus();
 		status.setResponseCode( HTTP_CODE_OK  );
 		status.setResponseDesc("Server still alive");
+		response.setStatus(status);
+		
+		return response;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/callback")
+	@ResponseStatus(HttpStatus.CREATED)
+	@ResponseBody
+	public XmlIvrResponse doPost(@RequestBody XmlIvrRequest request) {
+		
+		XmlIvrResponse response = new XmlIvrResponse();
+		response.setParams(null);
+		XmlIvrStatus status = new XmlIvrStatus();
+		
+		status.setResponseCode( HTTP_CODE_BAD_REQUEST  );
+		status.setResponseDesc("XML request is not well-form.");
+		
+		if ( request.getParams() != null ) {
+			if ( request.getParams().getParam() != null 
+					&& !request.getParams().getParam().isEmpty()  ) {
+				
+				for ( XmlIvrParam p : request.getParams().getParam() ) {
+					logger.info("Request: Name->" + p.getName() +", Value->"+p.getValue() );
+				}
+				
+				status.setResponseCode( HTTP_CODE_OK  );
+				status.setResponseDesc("Success");
+				
+			} else logger.warn("No found any XML element 'param' in XML request");
+		} else logger.warn("No found XML element 'params' in XML request");
+		
 		response.setStatus(status);
 		
 		return response;
