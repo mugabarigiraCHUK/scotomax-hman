4e388855960f25ce1dcf415dafda749d7afd7bc8
developmax@gmail.com <developmax@gmail.com@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-11-13 17:03:54 +0000

    Update codes

--- trunk/icall/app/controllers/AutoCallDistributors.java
+++ trunk/icall/app/controllers/AutoCallDistributors.java
@@ -11,11 +11,15 @@ import models.*;
 public class AutoCallDistributors extends Controller {
 
     public static void index() {
-        render("AutoCallDistributors/route.html");
+    	flash.success("You entered into ACD route management page successfully.");
+        List routes = AcdRoute.find("order by route_id desc").fetch();
+        render("AutoCallDistributors/route.html", routes);
     }
 
     public static void condition() {
-    	render("AutoCallDistributors/condition.html");
+    	flash.success("You entered into ACD condition management page successfully.");
+        List conditions = AcdCondition.find("order by condition_id desc").fetch();
+    	render("AutoCallDistributors/condition.html", conditions);
     }
 
     public static void route() {
