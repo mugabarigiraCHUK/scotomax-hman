e0f8d4c26d1ea35f80e4c2e885149324092fe179
developmax <developmax@gmail.com>
2011-11-15 18:35:23 +0000

    Update codes

--- icall/public/javascripts/acd.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	// Table sorter
-	$("table#acd-condition").tablesorter({ sortList: [[0,0], [1,0]], headers: { 2:{sorter: false}, 4:{sorter: false}, 5:{sorter: false}} });
-	// Modal panel
-	$("#modal-from-dom").modal({
-		keyboard: true
-	});
-
-});
