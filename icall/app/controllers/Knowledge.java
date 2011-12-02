e6157b3b27467f4d57547f5a5b75c8ed1153d95e
sevenG.oo7 <sevenG.oo7@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-12-02 14:20:40 +0000

    add new page Knowledge status

--- icall/app/controllers/Knowledge.java
+++ icall/app/controllers/Knowledge.java
@@ -23,7 +23,65 @@ public class Knowledge extends Controller {
 	}
 
 	public static void status() {
-		
+		List<KnwStatus> statusList = KnwStatus.find("order by status_id asc").fetch();
+		if (statusList.size() > 0) {
+			flash.success("Found "+statusList.size()+" result(s).");
+		}
+		render("knowledge/status.html", statusList);
 	}
+	
+	/**
+	* prepare status for save/update
+	* @param status_id
+	**/
+	public static void prepareStatus(Integer status_id) {
+		if (status_id != null) {
+			KnwStatus knwStatus = KnwStatus.findById(status_id);
+        	renderJSON(knwStatus);
+		}
+	}
+	
+	/**
+	* Save and update knwStatus
+	* @param status_id
+	* @param status_name
+	* @param status_description
+	**/
+	public static void saveStatus(Integer status_id, String status_name, String status_description) {
+		validation.required("Name", status_name);
+		
+		if (!validation.hasErrors()) {
+			if (status_id == null) {
+				KnwStatus knwStatus = new KnwStatus(null, null);
+				knwStatus.status_name = status_name;
+				knwStatus.status_description = status_description;
+				knwStatus.save();
+				flash.success("The data been created.");
+			} else {
+				KnwStatus knwStatus = KnwStatus.findById(status_id);
+				knwStatus.status_name = status_name;
+				knwStatus.status_description = status_description;
+				knwStatus.status_update_date = new Date();
+				knwStatus.save();
+				flash.success("The data been updated.");
+			}
+		}
+		
+		List<KnwStatus> statusList = KnwStatus.find("order by status_id asc").fetch();
+		render("knowledge/status.html", statusList);
+	}
+    
+    /**
+    * delete status
+    * @param delete_id
+    **/
+    public static void deleteStatus(@Required(message="No found ID to delete data.") Integer delete_id) {
+        KnwStatus knwStatus = KnwStatus.findById(delete_id);
+        knwStatus.delete();
+        flash.success("The data been deleted.");
+        
+        List<KnwStatus> statusList = KnwStatus.find("order by status_id asc").fetch();
+		render("knowledge/status.html", statusList);
+    }
 	
 }
