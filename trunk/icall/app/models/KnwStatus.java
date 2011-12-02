208ecddf95e18ede8031ae60fb97631796767437
sevenG.oo7 <sevenG.oo7@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2011-12-02 14:20:40 +0000

    add new page Knowledge status

--- trunk/icall/app/models/KnwStatus.java
+++ trunk/icall/app/models/KnwStatus.java
@@ -11,11 +11,14 @@ import play.data.validation.*;
 public class KnwStatus extends GenericModel {
  
  	@Id
- 	@GeneratedValue(strategy = GenerationType.AUTO)
+// 	@GeneratedValue(strategy = GenerationType.AUTO)
     public Integer status_id;
+    
     @Required
     public String status_name;
+    
     public String status_description;
+    
     @Required
     public Date status_update_date;
 
@@ -25,8 +28,9 @@ public class KnwStatus extends GenericModel {
     @OneToMany(mappedBy="knw_status_id")
     public List<KnwSolution> knwSolutions;
     
- 	public KnwStatus(Integer status_id, String status_name, String status_description){
- 		this.status_id = status_id;
+ 	public KnwStatus(String status_name, String status_description){
+ 		java.util.Random randomGenerator = new java.util.Random();
+    	this.status_id = randomGenerator.nextInt(1000000000);
  		this.status_name = status_name;
  		this.status_description = status_description;
  		this.status_update_date = new Date();
