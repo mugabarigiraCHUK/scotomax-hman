7441e25b1f35c17bd4c15e9c1fcb58febafc6dda
developmax <developmax@gmail.com>
2012-01-10 15:59:23 +0000

    Customize algorithm to apply the real concept of AMQP

--- rmq-agent/src/main/java/org/hman/amqp/AmqpUtil.java
+++ rmq-agent/src/main/java/org/hman/amqp/AmqpUtil.java
@@ -3,10 +3,13 @@ package org.hman.amqp;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.rabbitmq.client.Address;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -19,52 +22,103 @@ import com.rabbitmq.client.ConnectionFactory;
 public class AmqpUtil implements Serializable {
 
 	private static final long serialVersionUID = -3029137620640162611L;
-	private Logger slf4j = LoggerFactory.getLogger(AmqpUtil.class);
+	private Logger log = LoggerFactory.getLogger(AmqpUtil.class);
 	
 	private ConnectionFactory rmqConnFactory;
+	private ExecutorService es;
 	private Connection rmqConn;
 	private Channel rmqChannel;
 	private Map<String, String> configures;
 	
 	/**
-	 * Initial all component by configure parameters
-	 * 
+	 * Initial RabbitMQ objects/connections by configure parameters
+	 * - Mandatory
+	 * 		- RMQ_HOST
+	 * 		- RMQ_PORT
+	 * 		- RMQ_EXCHANGE
+	 * 		- RMQ_QUEUE
+	 * - If only if Exchange is not declare yet
+	 * 		- RMQ_EXCHANGE_TYPE [direct,topic,header,fanout]
+	 * - Optional
+	 * 		- RMQ_ROUTING_KEY ( Default is RMQ_QUEUE )
+	 * 		- RMQ_MIRROR_HOST
+	 * 		- RMQ_MIRROR_PORT
+	 * 		- RMQ_USERNAME
+	 * 		- RMQ_PASSWORD
+	 * 		- RMQ_CONN_POOL_SIZE ( Default is 1 )
+	 * 		- RMQ_CLIENT_TIMEOUT ( Default is 60s.)
+	 * 		- RMQ_PREFETCH_COUNT ( Default is 0 : Unlimit ) 
 	 * @param params
 	 * @return result code, 0 is success, otherwise is -1.
 	 */
 	public int init(Map<String, String> params) {
 		try {
 			
-			slf4j.debug("Copy configure parameter to local");
+			log.debug("Copy configure parameter to local");
 			configures = new HashMap<String, String>();
 			configures.putAll(params);
 			
-			slf4j.debug("Initaling RMQ connection factory");
+			log.debug("Initaling RMQ connection factory");
 			// Create connection factory
 			rmqConnFactory = new ConnectionFactory();
 			
-			slf4j.debug("Registering RMQ connection parameters");
+			log.debug("Registering RMQ connection parameters");
 			// Register server host.
-			rmqConnFactory.setHost(params.get(RMQ_HOST));
-			// Register server port. if existing
-			if ( params.containsKey(RMQ_PORT) )
-				rmqConnFactory.setPort(Integer.parseInt(params.get(RMQ_PORT)));
-			// Register server username for authentication. if existing.
-			if ( params.containsKey(RMQ_USERNAME) )
-				rmqConnFactory.setUsername(params.get(RMQ_USERNAME));
-			// Register server password for authentication. if existing.
-			if ( params.containsKey(RMQ_PASSWORD) )
-				rmqConnFactory.setPassword(params.get(RMQ_PASSWORD));
-			
-			slf4j.debug("Intialing RMQ connection");
-			// Create connection pipe
-		    rmqConn = rmqConnFactory.newConnection();
-		    
-		    slf4j.debug("Creating RMQ channel");
+			if ( params.containsKey( RMQ_MIRROR_HOST )
+				 && params.containsKey( RMQ_MIRROR_PORT )
+				 && params.containsKey( RMQ_PORT ) ) {
+				
+				// Register server username for authentication. if existing.
+				if ( params.containsKey(RMQ_USERNAME) )
+					rmqConnFactory.setUsername(params.get(RMQ_USERNAME));
+				// Register server password for authentication. if existing.
+				if ( params.containsKey(RMQ_PASSWORD) )
+					rmqConnFactory.setPassword(params.get(RMQ_PASSWORD));
+				
+				// Initial multiple broker address for clustering broker
+				Address[] addrArr 
+					= new Address[]{ new Address( params.get(RMQ_HOST), 
+												  Integer.parseInt( params.get(RMQ_PORT) ) )
+                				   , new Address( params.get(RMQ_MIRROR_HOST), 
+                					 	    	  Integer.parseInt( params.get(RMQ_MIRROR_PORT) ) ) };
+				
+				log.debug("Intialing RMQ connection");
+				// Create connection pipe
+				if ( params.containsKey(RMQ_CONN_POOL_SIZE) ) {
+					es = Executors.newFixedThreadPool( Integer.parseInt( params.get(RMQ_CONN_POOL_SIZE) ) );
+					rmqConn = rmqConnFactory.newConnection( es, addrArr ); 
+				} else {
+					es = Executors.newFixedThreadPool( 1 );
+					rmqConn = rmqConnFactory.newConnection( es, addrArr );
+				}
+				
+			} else {
+				rmqConnFactory.setHost(params.get(RMQ_HOST));
+				// Register server port. if existing
+				if ( params.containsKey(RMQ_PORT) )
+					rmqConnFactory.setPort(Integer.parseInt(params.get(RMQ_PORT)));
+				// Register server username for authentication. if existing.
+				if ( params.containsKey(RMQ_USERNAME) )
+					rmqConnFactory.setUsername(params.get(RMQ_USERNAME));
+				// Register server password for authentication. if existing.
+				if ( params.containsKey(RMQ_PASSWORD) )
+					rmqConnFactory.setPassword(params.get(RMQ_PASSWORD));
+				
+				log.debug("Intialing RMQ connection");
+				// Create connection pipe
+				if ( params.containsKey(RMQ_CONN_POOL_SIZE) ) {
+					es = Executors.newFixedThreadPool( Integer.parseInt( RMQ_CONN_POOL_SIZE ) );
+					rmqConn = rmqConnFactory.newConnection( es ); 
+				} else {
+					es = Executors.newFixedThreadPool( 1 );
+					rmqConn = rmqConnFactory.newConnection( es );
+				}
+			} 
+		    log.debug("Creating RMQ channel");
 		    // Open channel
 		    rmqChannel = rmqConn.createChannel();
 		    
-		    slf4j.debug("Declaring exchange on RMQ");
+		    log.debug("Declaring exchange on RMQ");
 		    // Declare the exchange name for used. if required
 		    /* Exchange type
 		     * - direct
@@ -72,26 +126,37 @@ public class AmqpUtil implements Serializable {
 		     * - header
 		     * - fanout
 		     */
-		    if ( params.containsKey(RMQ_EXCHANGE) )
+		    if ( params.containsKey(RMQ_EXCHANGE_TYPE) ) {
 		    	rmqChannel.exchangeDeclare( params.get(RMQ_EXCHANGE), params.get(RMQ_EXCHANGE_TYPE), true);
+		    }
 		    
-		    slf4j.debug("Declaring queue on RMQ");
+		    // log.debug("Declaring queue on RMQ");
 		    // Declare the queue name for receiving message. if require.
 		    String queueName = "";
 		    if ( params.containsKey(RMQ_QUEUE) )
 		    	queueName = params.get(RMQ_QUEUE);
 		    else {
-		    	queueName = rmqChannel.queueDeclare().getQueue();
+		     	queueName = rmqChannel.queueDeclare().getQueue();
 		    }
-		    rmqChannel.queueDeclare( queueName, true, false, false, null);
+		    // rmqChannel.queueDeclare( queueName, true, false, false, null);
 		    
-		    slf4j.debug("Binding queue with exchange");
+		    log.debug("Binding queue with exchange");
 		    // Binding queue and exchange which declared.
-		    rmqChannel.queueBind(queueName, params.get(RMQ_EXCHANGE), "");
+		    String routingkey = queueName;
+		    if ( params.containsKey( RMQ_ROUTING_KEY ) ) {
+		    	routingkey = params.get( RMQ_ROUTING_KEY );
+		    }
+		    rmqChannel.queueBind(queueName, params.get(RMQ_EXCHANGE), routingkey);
+		    
+		    // Setting QoS for limit maximum number of messages that the server will deliver
+		    if ( params.containsKey( RMQ_PREFETCH_COUNT ) ) {
+		    	int prefetchCount = Integer.parseInt( params.get( RMQ_PREFETCH_COUNT ) );
+		    	rmqChannel.basicQos( prefetchCount );
+		    }
 			
 			return 0;
 		} catch (Exception ex) {
-			slf4j.warn("Failed to initial Cmi object, "+ex.getMessage(), ex);
+			log.warn("Failed to initial Cmi object, "+ex.getMessage(), ex);
 			return -1;
 		}
 	}
@@ -101,15 +166,15 @@ public class AmqpUtil implements Serializable {
 	 */
 	public void destroy() {
 		try {
-			slf4j.debug("Closing RMQ channel");
+			log.debug("Closing RMQ channel");
 			// Close channel
 			rmqChannel.close();
 			
-			slf4j.debug("Closing RMQ connection");
+			log.debug("Closing RMQ connection");
 			// Close connection
 			rmqConn.close();
 		} catch (Exception ex) {
-			slf4j.warn("Failed to destroy Cmi object, "+ex.getMessage(), ex);
+			log.warn("Failed to destroy Cmi object, "+ex.getMessage(), ex);
 		}
 	}
 	
@@ -125,21 +190,37 @@ public class AmqpUtil implements Serializable {
 		else return false;
 	}
 	
+	/**
+	 * Getting AMQP channel for utility
+	 * 
+	 * @return instance of AMQP channel
+	 */
 	public Channel getChannel() {
 		return rmqChannel;
 	}
 	
-	public Map<String, String> getConfigures(){
+	/**
+	 * Getting configuration properties
+	 * 
+	 * @return List of mapped name:value of properties
+	 */
+	public final Map<String, String> getConfigures(){
 		return configures;
 	}
 	
-	
 	public static final String RMQ_HOST = "RMQ_HOST";
 	public static final String RMQ_PORT = "RMQ_PORT";
+	public static final String RMQ_MIRROR_HOST = "RMQ_MIRROR_HOST";
+	public static final String RMQ_MIRROR_PORT = "RMQ_MIRROR_PORT";
+	
 	public static final String RMQ_EXCHANGE = "RMQ_EXCHANGE";
 	public static final String RMQ_QUEUE = "RMQ_QUEUE";
+	public static final String RMQ_ROUTING_KEY = "RMQ_ROUTING_KEY";
 	public static final String RMQ_USERNAME = "RMQ_USERNAME";
 	public static final String RMQ_PASSWORD = "RMQ_PASSWORD";
+	public static final String RMQ_CONN_POOL_SIZE = "RMQ_CONN_POOL_SIZE";
+	public static final String RMQ_CLIENT_TIMEOUT = "RMQ_CLIENT_TIMEOUT";
+	public static final String RMQ_PREFETCH_COUNT = "RMQ_PREFETCH_COUNT";
 	
 	public static final String RMQ_EXCHANGE_TYPE = "EXCHANGE_TYPE";
 	public static final String RMQ_EXCHANGE_DIRECT = "direct";
