f5df89a44d40be651cadf5b3159605fdea73378c
gmalipen <gmalipen@gmail.com>
2012-07-16 08:03:29 +0000

    แก้ไข text หน้า login ที่พิมผิด

--- proj21/dbms/login.php
+++ proj21/dbms/login.php
@@ -17,11 +17,6 @@
         <div class="span3">
           	<div class="well sidebar-nav">
 		        <ul class="nav nav-list">
-			
-				<!--@Start ######### Navigation panel editor place ########## -->
-				
-				
-				<!--@End ######### Navigation panel editor place ########## -->
 		
 		        </ul>
 		      </div><!--/.well -->
