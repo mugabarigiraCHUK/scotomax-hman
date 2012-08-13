e0fdca1486b0c036085edb15170baba75d08f646
developmax@gmail.com <developmax@gmail.com@ad344d4b-5b83-fe1d-98cd-db9ebd70f650>
2012-08-13 16:35:46 +0000

    Correct java package and configuration.

---
+++ trunk/jsf2rf4/src/main/java/org/cathod/sample/jsf2rf4/bean/HomeBean.java
@@ -0,0 +1,28 @@
+package org.cathod.sample.jsf2rf4.bean;
+
+public abstract class HomeBean {
+
+	protected String username;
+	protected String password;
+	protected String message;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
